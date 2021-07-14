f52e8509fedd6eb5ab9951d68029935ef433ccdd
kortkamp <marcelusmedius@gmail.com>
2021-07-13 22:37:36 -0300

    refactor: change app to typescript

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import { apiRoutes } from "./routes";
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req:Request, res:Response, next:NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err:any, req:Request, res:Response, next:NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500).send();
+  
+});
+
+module.exports = app;
