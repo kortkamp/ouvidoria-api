e34dfa7119dd87e4d85420ed75d212cd8aa030ab
kortkamp <marcelusmedius@gmail.com>
2021-07-15 22:47:26 -0300

    fix: api ok reponse

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ const apiRoutes = Router();
 
 const createUserController = new CreateUserController();
 
-apiRoutes.get('/', (_, res: Response) => res.send('ok'));
+apiRoutes.get('/', (_, res: Response) => res.send('api ok'));
 apiRoutes.post('/users', createUserController.handle);
 
 // eslint-disable-next-line import/prefer-default-export
