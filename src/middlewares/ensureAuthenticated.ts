9c557497abd6e5b7d01ccb2114380d78d90a0321
kortkamp <marcelusmedius@gmail.com>
2021-07-17 23:25:42 -0300

    refactor: separate atachUser from ensurteAuth

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,31 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-import { verify } from 'jsonwebtoken';
-
-import config from '../config';
-
-interface IPayload {
-  sub:string
-}
 
 export default function EnsureAuthenticated(
   request:Request,
   response:Response,
   next:NextFunction,
 ) {
-  const authToken = request.headers.authorization;
+  const { user_id } = request;
 
-  if (!authToken) {
+  if (!user_id) {
     return response.status(401).end();
-  }
-
-  const [, token] = authToken.split(' ');
-
-  try {
-    const { sub } = verify(token, config.jwtSecret) as IPayload;
-
-    request.user_id = sub;
-  } catch (err) {
-    response.status(401).end();
   }
 
   return next();
