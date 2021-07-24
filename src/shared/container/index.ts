67d7848ef479c5aec827201bbd8ec98079e91a58
kortkamp <marcelusmedius@gmail.com>
2021-07-23 23:04:51 -0300

    fix: add inject to Districts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IDistricstRepository from '@modules/districts/repositories/IDistrictRepository';
+import DistrictsRepository from '@modules/districts/infra/typeorm/repositories/DistrictsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IDistricstRepository>(
+  'DistrictsRepository',
+  DistrictsRepository,
 );
