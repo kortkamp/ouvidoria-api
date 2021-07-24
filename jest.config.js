84aa7ac8b60c2f652718e8c46f1748917aa2d03f
kortkamp <marcelusmedius@gmail.com>
2021-07-24 17:01:18 -0300

    chore: configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,20 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/services/*.ts'
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  'roots': [
+    '<rootDir>/src'
+  ],
+  'transform': {
+      '^.+\\.tsx?$': 'ts-jest'
+  },
+  'moduleNameMapper': {
+      '@modules/(.*)': '<rootDir>/src/modules/$1',
+      '@shared/(.*)': '<rootDir>/src/shared/$1',
+      '@config/(.*)': '<rootDir>/src/config/$1'
+      
+  }
 };
