88833305aaf6de6ed73b4f82d436cbdb372a5aa1
kortkamp <marcelusmedius@gmail.com>
2021-07-25 09:37:02 -0300

    fix: add import reflect-metadata

--- src/modules/districts/services/CreateDistrictService.ts
+++ src/modules/districts/services/CreateDistrictService.ts
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
+import 'reflect-metadata';
 import IDistrictsRepository from '@modules/districts/repositories/IDistrictRepository';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
