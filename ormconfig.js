7ee7ea2618d63e37ed791a6dff84bcd1f7b6a7d7
kortkamp <marcelusmedius@gmail.com>
2021-07-26 16:25:54 -0300

    chore: configure multiple envs

--- ormconfig.js
+++ ormconfig.js
@@ -3,9 +3,15 @@ require('dotenv/config');
 
 const database = {
   dev: "ouvidoria_dev",
-  test: 'ouvidoria_test'
+  test: 'ouvidoria_test',
+  dist: 'ouvidoria_dev'
 }
 
+const entitiesDir = {
+  dev: "./src/modules/**/infra/typeorm/entities/*.{ts,js}",
+  test: "./src/modules/**/infra/typeorm/entities/*.{ts,js}",
+  dist: "dist/modules/**/infra/typeorm/entities/*.{ts,js}"
+}
 
 module.exports = {
   name:"default",
@@ -21,9 +27,7 @@ module.exports = {
   
   migrations:["./shared/infra/typeorm/migrations/*.ts"],
   entities:[
-    //"/home/marcelo/dev/ouvidoria-api/src/modules/**/infra/typeorm/entities/*.{ts,js}"
-    //"dist/modules/**/infra/typeorm/entities/*.{ts,js}"
-    "./src/modules/**/infra/typeorm/entities/*.{ts,js}"
+    entitiesDir[process.env.NODE_ENV || 'dev'],
   ],
   cli:{
     "migrationsDir": "src/shared/infra/typeorm/migrations",
