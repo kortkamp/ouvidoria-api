e3a2fe58fca8703b3c911e4a19418a110b16939d
kortkamp <marcelusmedius@gmail.com>
2021-07-27 10:49:34 -0300

    test: create delete complaint service test

---
+++ src/__test__/unit/complaints/DeleteComplaintsService.test.ts
@@ -0,0 +1,57 @@
+import DeleteComplaintService from '@modules/complaints/services/DeleteComplaintService';
+import FakeComplaintsRepository from '@modules/complaints/repositories/fakes/FakeComplaintsRepository';
+import FakeDistrictsRepository from '@modules/districts/repositories/fakes/FakeDistrictsRepository';
+import ICreateComplaintDTO from '@modules/complaints/dtos/ICreateComplaintDTO';
+import { District } from '@modules/districts/infra/typeorm/entities/District';
+import { Complaint } from '@modules/complaints/infra/typeorm/entities/Complaint';
+
+let fakeComplaintsRepository:FakeComplaintsRepository;
+let fakeDistrictsRepository:FakeDistrictsRepository;
+let deleteComplaintService:DeleteComplaintService;
+let districtName:string;
+let district:District;
+let createComplaintData:ICreateComplaintDTO;
+let complaint: Complaint;
+
+describe('DeleteComplaintsService', () => {
+  beforeEach(async () => {
+    fakeComplaintsRepository = new FakeComplaintsRepository();
+    fakeDistrictsRepository = new FakeDistrictsRepository();
+
+    deleteComplaintService = new DeleteComplaintService(
+      fakeComplaintsRepository,
+      fakeDistrictsRepository,
+    );
+
+    districtName = 'districtName';
+
+    district = await fakeDistrictsRepository.create(districtName);
+
+    createComplaintData = {
+      district_id: district.id,
+      message: `Some random complaint about ${district.name}`,
+      user_sender: '1',
+    };
+
+    complaint = await fakeComplaintsRepository.create(createComplaintData);
+  });
+
+  it('Should not allow an user to delete someones else complaint', async () => {
+    await expect(
+      deleteComplaintService.execute({
+        complaint_id: complaint.id,
+        user_sender: 'someone_use_id',
+      }),
+    ).rejects.toMatchObject({ statusCode: 401 });
+  });
+
+  it('Should be able to delete a complaint', async () => {
+    await expect(
+      deleteComplaintService.execute({
+        complaint_id: complaint.id,
+        user_sender: complaint.user_sender,
+      }),
+
+    ).resolves.toBeUndefined();
+  });
+});
