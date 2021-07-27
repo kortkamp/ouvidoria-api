2475b4e46c983fd03c9db5cb8b8e6bfd14e49198
kortkamp <marcelusmedius@gmail.com>
2021-07-27 10:19:29 -0300

    feat: create delete complaint service

---
+++ src/modules/complaints/services/DeleteComplaintService.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-throw-literal */
+/* eslint-disable camelcase */
+import 'reflect-metadata';
+import IComplaintsRepository from '@modules/complaints/repositories/IComplaintsRepository';
+import IDistrictsRepository from '@modules/districts/repositories/IDistrictRepository';
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import IDeleteComplaintDTO from '../dtos/IDeleteComplaintDTO';
+
+@injectable()
+class DeleteComplaintService {
+  constructor(
+    @inject('ComplaintsRepository')
+    private complaintsRepository: IComplaintsRepository,
+    @inject('DistrictsRepository')
+    private districtsRepository: IDistrictsRepository,
+  ) {}
+
+  async execute({
+    complaint_id, user_sender,
+  }: IDeleteComplaintDTO) {
+    // must check if user_sender is admin or complaint author
+    const complaint = await this.complaintsRepository.findById(
+      complaint_id,
+    );
+    if (complaint?.user_sender !== user_sender) {
+      throw new AppError('User is not the complaint author', 401);
+    }
+    await this.complaintsRepository.delete(
+      complaint_id,
+    );
+    return complaint;
+  }
+}
+export default DeleteComplaintService;
