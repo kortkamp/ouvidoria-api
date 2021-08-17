5c606f610b0188071af1c7747b5cb5fcfdcf2680
kortkamp <marcelusmedius@gmail.com>
2021-08-17 15:35:31 -0300

    feat: change default port to 3333

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`🚀 Server started on port ${port}!`);
