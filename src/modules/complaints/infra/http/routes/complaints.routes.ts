4a2fd16cb4f98315b5c100af956e62488b33ecc5
kortkamp <marcelusmedius@gmail.com>
2021-08-24 21:51:40 -0300

    feat: add route to list complaints by district

--- src/modules/complaints/infra/http/routes/complaints.routes.ts
+++ src/modules/complaints/infra/http/routes/complaints.routes.ts
@@ -3,15 +3,18 @@ import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthentica
 
 import CreateComplaintController from '@modules/complaints/infra/http/controllers/CreateComplaintController';
 import ListComplaintsController from '@modules/complaints/infra/http/controllers/ListComplaintsController';
+import ListComplaintsByDistrictController from '../controllers/ListComplaintsByDistrictController';
 
 const route = Router();
 
 const createComplaintController = new CreateComplaintController();
 const listComplaintsController = new ListComplaintsController();
+const listComplaintsByDistrictController = new ListComplaintsByDistrictController();
 
 export default (app: Router) => {
   app.use('/complaints', route);
 
   route.get('/', ensureAuthenticated, listComplaintsController.handle);
   route.post('/', ensureAuthenticated, createComplaintController.handle);
+  route.get('/district/:district_id', listComplaintsByDistrictController.handle);
 };
